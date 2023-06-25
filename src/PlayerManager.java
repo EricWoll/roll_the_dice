cffbe2069ac130815c2be6efb3807e0e4b6f2f9d
Steelheart96 <steelheart696@gmail.com>
2023-06-25 14:21:48 -0700

    Cleaned up files.

--- src/PlayerManager.java
+++ src/PlayerManager.java
@@ -1,4 +1,3 @@
-import java.util.Enumeration;
 import java.util.Map;
 import java.util.HashMap;
 
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,6 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import jdk.jfr.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 class PlayerTest {
